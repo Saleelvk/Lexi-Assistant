a82e6217cfecf64d90544056584715eb0097c469
Saleelvk <saleelvkshemi@gmail.com>
2025-07-11 10:17:54 +0530

    change

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 import {
   Search,
@@ -58,7 +58,7 @@ const App = () => {
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-slate-900 via-purple-900 to-slate-900 relative overflow-hidden">
-       {/* animations-part */}
+      {/* animations-part */}
       <div className="absolute inset-0 overflow-hidden">
         <div className="absolute -top-40 -right-40 w-80 h-80 bg-purple-500 rounded-full opacity-20 animate-pulse"></div>
         <div className="absolute -bottom-40 -left-40 w-80 h-80 bg-blue-500 rounded-full opacity-20 animate-pulse"></div>
@@ -82,7 +82,7 @@ const App = () => {
           ></div>
         ))}
       </div>
-       {/* animations-End */}
+      {/* animations-End */}
       <div className="relative z-10 min-h-screen  font-inter p-6 flex items-center justify-center">
         <div className="max-w-4xl w-full">
           {/* Header */}
